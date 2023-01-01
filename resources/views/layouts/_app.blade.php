f9d61341adb5c2656d145443a37e25e1995b5b7d
omar96i <sagara9930@hotmail.com>
2023-01-01 10:01:26 -0500

    update

--- resources/views/layouts/_app.blade.php
+++ resources/views/layouts/_app.blade.php
@@ -14,7 +14,7 @@
       content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"
     />
 
-    <title>CRM - @yield('title')</title>
+    <title>ERP - @yield('title')</title>
 
     @include('layouts._head')
     @yield('css')
